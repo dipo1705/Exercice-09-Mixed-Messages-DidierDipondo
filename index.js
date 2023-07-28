3855c0ce453fa0418636457197fe29896fb93e7b
dipo1705 <didier.dipondo@gmail.com>
2023-07-28 16:24:07 +0100

    feat : add index file

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//hello Didier
+//hello Didier ceci est un programme de l'Horoscope
+const phrase = ['Attendez vous à une journée merveilleuse. Un voyage professionel risque de mal tourné. Une bonne gestion de vos finances est garantit'];
+
+//cette fonction genère un message d'horoscope
+const randomHoroscope = signHoroscope =>{
+    const phraseIndex = Math.floor(Math.random()*phrase.length);
+
+    console.log(`${signHoroscope} : ${phrase[phraseIndex]}`)
+};
+//Le signe de l'utilisateur doit être remplacé par son propre signe
+let sign = 'Scorpion';
+randomHoroscope(sign);
+
